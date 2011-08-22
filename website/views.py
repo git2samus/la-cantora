30b8414159b26b6c3198d2f7d6aefb57efcc3826
Samus_ <mail2samus@gmail.com>
2011-08-22 04:14:47 -0300

    adding search form (GET) and context_dict helper

--- website/views.py
+++ website/views.py
@@ -1,7 +1,16 @@
 from google.appengine.ext.webapp import RequestHandler, template
 
+# helper to initialize template context from locals()
+def context_dict(locals_dict, *varnames, **overrides):
+    context = {'request': locals_dict.get('request')}
+    context.update((varname, locals_dict.get(varname)) for varname in varnames)
+    context.update(overrides)
+    return context
+
 class WebsiteHandler(RequestHandler):
     def get(self):
-        page = template.render('templates/index.html', {})
+        q = self.request.get('q')
+
+        page = template.render('templates/index.html', context_dict(locals(), 'result'))
         self.response.out.write(page)
 
