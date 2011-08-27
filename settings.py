5c88ff95747d9ab41fa5f07cf6e22dd0e9f7f59f
Samus_ <mail2samus@gmail.com>
2011-08-27 04:54:12 -0300

    adding gdata library

--- settings.py
+++ settings.py
@@ -3,10 +3,12 @@ from os import path
 
 DEBUG = False
 
+APPROOT = path.dirname(path.abspath(__file__))
+
 # YOUTUBE_API_KEY = ''
 
 # sensitive and deploy-specific settings (untracked)
-local_settings_path = path.join(path.dirname(path.abspath(__file__)), 'local_settings.py')
+local_settings_path = path.join(APPROOT, 'local_settings.py')
 if path.exists(local_settings_path):
     # use execfile to allow modifications within this context
     execfile(local_settings_path)
--- website/__init__.py
+++ website/__init__.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python
+import os, sys, settings
+sys.path.append(os.path.join(settings.APPROOT, 'lib', 'python'))
 
 from google.appengine.dist import use_library
 use_library('django', '1.2')
@@ -6,7 +8,6 @@ use_library('django', '1.2')
 from google.appengine.ext.webapp import WSGIApplication
 from google.appengine.ext.webapp.util import run_wsgi_app
 from website.views import WebsiteHandler
-import settings
 
 def main():
     urlpatterns = [
